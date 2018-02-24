70c6f7ed1225dd5c54b5719f772d3c3d6eb27fc9
adorcy <adorcy@unomaha.edu>
2018-02-24 04:26:20 -0600

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!M:\School\UNO Spring 2018\Distributed systems\Assignment 2\financial1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
