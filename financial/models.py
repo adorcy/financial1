22092dcd772042fb4be1b4a3c76eb283de7c8e49
adorcy <adorcy@unomaha.edu>
2018-02-24 14:59:45 -0600

    Add methods to models
    Added methods to models based

--- financial/models.py
+++ financial/models.py
@@ -11,6 +11,12 @@ class Customer(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=11)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
 
 class Stock(models.Model):
     id = models.AutoField(primary_key=True)
@@ -21,6 +27,13 @@ class Stock(models.Model):
     purchase_price = models.DecimalField(max_digits=None, decimal_places=2)
     date_purchased = models.DateTimeField(blank=True, null=True)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
 class Cryptocurrency(models.Model):
     id = models.AutoField(primary_key=True)
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
@@ -28,3 +41,10 @@ class Cryptocurrency(models.Model):
     number_of_coins = models.IntegerField()
     purchase_price = models.DecimalField(max_digits=None, decimal_places=2)
     date_purchased = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
