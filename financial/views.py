0915e0cc146a5a1e8a1bd0abdf0087e1bf2dd432
adorcy <adorcy@unomaha.edu>
2018-02-24 21:00:46 -0600

    Added Cryptocurrency and Stock Details pages

--- financial/views.py
+++ financial/views.py
@@ -15,3 +15,10 @@ def customer_detail(request, pk):
     cryptocurrencies = Cryptocurrency.objects.filter(created__lte=timezone.now()).order_by('date_purchased')
     return render(request, 'financial/customer_detail.html', {'customer': customer, 'stocks': stocks,  'cryptocurrencies': cryptocurrencies},)
 
+def stock_detail(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    return render(request, 'financial/stock_detail.html', {'stock': stock})
+
+def cryptocurrency_detail(request, pk):
+    cryptocurrency = get_object_or_404(Cryptocurrency, pk=pk)
+    return render(request, 'financial/cryptocurrency_detail.html', {'cryptocurrency': cryptocurrency})
