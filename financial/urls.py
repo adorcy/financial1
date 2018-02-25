a1ecd7d826a482c166ac539720ec88fdba1101f7
adorcy <adorcy@unomaha.edu>
2018-02-25 15:38:32 -0600

    Added Edit Features

--- financial/urls.py
+++ financial/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     url(r'^cryptocurrency/(?P<pk>\d+)/$', views.cryptocurrency_detail, name='cryptocurrency_detail'),
     url(r'^customer/new/$', views.customer_new, name='customer_new'),
     url(r'^customer/(?P<pk>\d+)/edit/$', views.customer_edit, name='customer_edit'),
+    url(r'^stock/new/$', views.stock_new, name='stock_new'),
+    url(r'^cryptocurrency/new/$', views.cryptocurrency_new, name='cryptocurrency_new'),
+    url(r'^stock/(?P<pk>\d+)/edit/$', views.stock_edit, name='stock_edit'),
+    url(r'^cryptocurrency/(?P<pk>\d+)/edit/$', views.cryptocurrency_edit, name='cryptocurrency_edit'),
 ]
--- financial/views.py
+++ financial/views.py
@@ -4,6 +4,8 @@ from .models import Customer
 from .models import Stock
 from .models import Cryptocurrency
 from .forms import CustomerForm
+from .forms import StockForm
+from .forms import CryptocurrencyForm
 from django.shortcuts import redirect
 
 def customer_list(request):
@@ -49,3 +51,57 @@ def customer_edit(request, pk):
     else:
         form = CustomerForm(instance=customer)
     return render(request, 'financial/customer_edit.html', {'form': form})
+
+def stock_new(request):
+    form = StockForm()
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.is_valid():
+            stock = form.save(commit=False)
+            stock.created = timezone.now()
+            stock.date_purchased = timezone.now()
+            stock.save()
+            return redirect('stock_detail', pk=stock.pk)
+    else:
+        form = StockForm()
+    return render(request, 'financial/stock_edit.html', {'form': form})
+
+def cryptocurrency_new(request):
+    form = CryptocurrencyForm()
+    if request.method == "POST":
+        form = CryptocurrencyForm(request.POST)
+        if form.is_valid():
+            cryptocurrency = form.save(commit=False)
+            cryptocurrency.created = timezone.now()
+            cryptocurrency.date_purchased = timezone.now()
+            cryptocurrency.save()
+            return redirect('cryptocurrency_detail', pk=cryptocurrency.pk)
+    else:
+        form = CryptocurrencyForm()
+    return render(request, 'financial/cryptocurrency_edit.html', {'form': form})
+
+def stock_edit(request, pk):
+    stock = get_object_or_404(Stock, pk=pk)
+    if request.method == "POST":
+        form = StockForm(request.POST, instance=stock)
+        if form.is_valid():
+            stock = form.save(commit=False)
+            stock.modified = timezone.now()
+            stock.save()
+            return redirect('stock_detail', pk=stock.pk)
+    else:
+        form = StockForm(instance=stock)
+    return render(request, 'financial/stock_edit.html', {'form': form})
+
+def cryptocurrency_edit(request, pk):
+    cryptocurrency = get_object_or_404(Cryptocurrency, pk=pk)
+    if request.method == "POST":
+        form = CryptocurrencyForm(request.POST, instance=cryptocurrency)
+        if form.is_valid():
+            cryptocurrency = form.save(commit=False)
+            cryptocurrency.modified = timezone.now()
+            cryptocurrency.save()
+            return redirect('cryptocurrency_detail', pk=cryptocurrency.pk)
+    else:
+        form = CryptocurrencyForm(instance=cryptocurrency)
+    return render(request, 'financial/cryptocurrency_edit.html', {'form': form})
